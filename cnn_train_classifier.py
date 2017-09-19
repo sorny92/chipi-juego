7e56b78e5108cfa6c229de7b683c76e67b672c3f
Esteve Soria <esofabian@gmail.com>
2017-09-19 19:50:09 +0200

    - Images uploaded - cnn classifier updated for the game - THERE'S ERRORS xecuting the trainning

--- cnn_train_classifier.py
+++ cnn_train_classifier.py
@@ -13,48 +13,62 @@ from keras.preprocessing.image import ImageDataGenerator
 
 from sklearn.model_selection import train_test_split
 
-image_names_car = glob.glob('./vehicles/*/*')
-image_names_no_car = glob.glob('./no-car64/*')
+images_background = glob.glob('./images/background/*')
+images_ball = glob.glob('./images/ball/*')
+images_box = glob.glob('./images/box/*')
+images_hole = glob.glob('./images/hole/*')
+images_rock = glob.glob('./images/rock/*')
+background_features = []
+ball_features = []
+box_features = []
+hole_features = []
+rock_features = []
 
-car_features = []
-no_car_features = []
-
-print('loading images 1')
-for name in image_names_car:
+for name in images_background:
     image = cv2.imread(name)
+    image = cv2.resize(image, (24,24))
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    car_features.append(image)
-print('loading images 2')
-for name in image_names_no_car:
+    background_features.append(image)
+    
+for name in images_ball:
     image = cv2.imread(name)
+    image = cv2.resize(image, (24,24))
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    no_car_features.append(image)
-print('Loaded')
+    ball_features.append(image)
+for name in images_box:
+    image = cv2.imread(name)
+    image = cv2.resize(image, (24,24))
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    box_features.append(image)
+for name in images_hole:
+    image = cv2.imread(name)
+    image = cv2.resize(image, (24,24))
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    hole_features.append(image)
+for name in images_rock:
+    image = cv2.imread(name)
+    image = cv2.resize(image, (24,24))
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    rock_features.append(image)
 
-car_features = sklearn.utils.shuffle(car_features)
-car_features = car_features[:4000]
-y = np.hstack((np.ones(len(car_features)),np.zeros(len(no_car_features))))
-X = np.vstack((car_features, no_car_features)).astype(np.float64)
-print(X[0].shape)
+y = np.hstack((
+    np.ones(len(background_features)), 
+    np.ones(len(ball_features)), 
+    np.ones(len(box_features)), 
+    np.ones(len(hole_features)), 
+    np.ones(len(rock_features))))
+#y.reshape((-1,1))
+X = np.vstack((background_features, ball_features, box_features, hole_features, rock_features)).astype(np.float64)
+print(X.shape)
+print(y.shape)
 X, y = sklearn.utils.shuffle(X, y)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-def generator(x, y, batch_size=32):
-    num_samples = len(samples)
-    while 1: # Loop forever so the generator never terminates
-        for offset in range(0, num_samples, batch_size):
-            X_batch = x[offset:offset+batch_size]
-            y_batch = y[offset:offset+batch_size]
-            yield sklearn.utils.shuffle(X_batch, y_batch)
+BATCH_SIZE = 2
+EPOCHS = 10
 
-BATCH_SIZE = 32
-EPOCHS = 3
-
-train_generator = generator(X_train, y_train, batch_size=BATCH_SIZE)
-validation_generator = generator(X_test, y_test, batch_size=BATCH_SIZE)
-
-ch, row, col = 3, 64, 64
+ch, row, col = 3, 24, 24
 
 def preprocess(x):
     x = x/127.5 - 1
@@ -64,31 +78,34 @@ model = Sequential()
 model.add(Lambda(preprocess, input_shape=(row, col, ch)))
 
 model.add(Conv2D(24, (5, 5)))
-model.add(MaxPooling2D((2, 2)))
 model.add(Activation('relu'))
-model.add(Dropout(0.4))
+#model.add(Dropout(0.4))
 
 model.add(Conv2D(36, (5, 5)))
-model.add(MaxPooling2D((2, 2)))
 model.add(Activation('relu'))
-model.add(Dropout(0.4))
+#model.add(Dropout(0.4))
 
 model.add(Conv2D(48, (3, 3)))
-model.add(MaxPooling2D((2, 2)))
 model.add(Activation('relu'))
-model.add(Dropout(0.4))
+#model.add(Dropout(0.4))
 
 model.add(Conv2D(64, (3, 3)))
 model.add(Activation('relu'))
-model.add(Dropout(0.4))
+#model.add(Dropout(0.4))
 
-model.add(Flatten())
-model.add(Dense(50))
-model.add(Dropout(0.4))
-model.add(Dense(10))
-model.add(Dense(1))
+model.add(Conv2D(64, (3, 3), strides=(2,2)))
 model.add(Activation('relu'))
 
-model.compile(loss='mse', optimizer='adam')
-model.fit(X, y, batch_size = BATCH_SIZE, epochs=EPOCHS, validation_split=0.2)
-model.save('model_v2.h5')
+model.add(Flatten())
+model.add(Dense(40))
+#model.add(Dropout(0.4))
+model.add(Dense(10))
+model.add(Dense(5))
+model.add(Activation('relu'))
+
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+model.fit(X_train, y_train, batch_size = BATCH_SIZE, epochs=EPOCHS, validation_split=0.2)
+score = model.evaluate(X_test, y_test, batch_size=BATCH_SIZE)
+print (score)
+print (model.predict(X_test[1,:]))
+model.save('model.h5')
